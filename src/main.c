8b20d39e60e3ba422a22698c5108c40f0cb96c66
Patricia Quesada Duenas <patquesa@c1r4s3.42malaga.com>
2026-01-21 11:01:50 +0100

    norma

--- src/main.c
+++ src/main.c
@@ -6,38 +6,37 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 10:13:49 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/21 10:20:30 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/21 10:59:07 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <MLX42/MLX42.h>
 #include "cub3d.h"
 
-//ESC //cuando MLX detecta la pulsación de una tecla, llama a key_hook
+//ESC //param = &game (direccion estructura game) //hook siempre recibe puntero a estructura
 static void	key_hook(mlx_key_data_t keydata, void *param)
 {
-	mlx_t	*mlx;
+	t_game	*game;
 
-	mlx = (mlx_t *)param;
+	game = (t_game *)param;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-		mlx_close_window(mlx);
+		mlx_close_window(game->mlx);
 }
 
-int main(void)
+int	main(void)
 {
-	mlx_t		*mlx;
-	mlx_image_t	*img;
+	t_game	game;
 
-	mlx = mlx_init(W, H, "cub3D (MLX42 Linux)", true); //estructura interna (mlx_t)
-	if (!mlx)
+	game.mlx = mlx_init(W, H, "cub3D", false); //inicializa MLX42 y crea la ventana
+	if (!game.mlx)
 		return (1); //Devuelve un puntero a esa estructura (puntero mlx)
-	img = mlx_new_image(mlx, W, H);
-	if (!img)
-		return (mlx_terminate(mlx), 1);
-	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
-		return (mlx_terminate(mlx), 1);
-	mlx_key_hook(mlx, key_hook, mlx); //le dices q cuando llames a key_hook, pasale mlx como parametro
-	mlx_loop(mlx); //permanece esperando hasta que pasa algo, como pulsar tecla
-	mlx_terminate(mlx); //limpia y cierra todo lo que se creo con MLX
+	game.img = mlx_new_image(game.mlx, W, H);
+	if (!game.img)
+		return (mlx_terminate(game.mlx), 1);
+	if (mlx_image_to_window(game.mlx, game.img, 0, 0) < 0)
+		return (mlx_terminate(game.mlx), 1);
+	mlx_key_hook(game.mlx, key_hook, &game); //le dices q cuando llames a key_hook, pasale estructura game
+	mlx_loop(game.mlx); //permanece esperando hasta que pasa algo, como pulsar tecla
+	mlx_terminate(game.mlx); //limpia y cierra todo lo que se creo con MLX
 	return (0);
 }
