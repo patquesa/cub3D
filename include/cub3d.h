54b1bd8a81e8484eaa2763f9384baecc1a30a75e
Patricia Quesada Duenas <patquesa@c1r4s3.42malaga.com>
2026-01-21 10:22:01 +0100

    norma

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 10:21:09 by patquesa          #+#    #+#             */
+/*   Updated: 2026/01/21 10:21:20 by patquesa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 
 # include <MLX42/MLX42.h>
 # include <stdint.h>
 
-# define W 800
+# define W 800  //tamaño de la ventana
 # define H 600
 //t_game es el estado global del juego
 typedef struct s_game
--- src/main.c
+++ src/main.c
@@ -1,21 +1,41 @@
-#include <MLX42/MLX42.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 10:13:49 by patquesa          #+#    #+#             */
+/*   Updated: 2026/01/21 10:20:30 by patquesa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define W 800 //tamaño de la ventana
-#define H 600
+#include <MLX42/MLX42.h>
+#include "cub3d.h"
+
 //ESC //cuando MLX detecta la pulsación de una tecla, llama a key_hook
-static void key_hook(mlx_key_data_t keydata, void *param)
+static void	key_hook(mlx_key_data_t keydata, void *param)
 {
-	mlx_t *mlx = (mlx_t *)param;
+	mlx_t	*mlx;
+
+	mlx = (mlx_t *)param;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		mlx_close_window(mlx);
 }
 
 int main(void)
 {
-	mlx_t *mlx = mlx_init(W, H, "cub3D (MLX42 Linux)", true); //estructura interna (mlx_t)
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+
+	mlx = mlx_init(W, H, "cub3D (MLX42 Linux)", true); //estructura interna (mlx_t)
 	if (!mlx)
 		return (1); //Devuelve un puntero a esa estructura (puntero mlx)
-
+	img = mlx_new_image(mlx, W, H);
+	if (!img)
+		return (mlx_terminate(mlx), 1);
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+		return (mlx_terminate(mlx), 1);
 	mlx_key_hook(mlx, key_hook, mlx); //le dices q cuando llames a key_hook, pasale mlx como parametro
 	mlx_loop(mlx); //permanece esperando hasta que pasa algo, como pulsar tecla
 	mlx_terminate(mlx); //limpia y cierra todo lo que se creo con MLX
