84f4441b567e73e7213729398032440d9d21a152
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-01-23 19:22:31 +0100

    norma

--- src/parser/find_spawn.c
+++ src/parser/find_spawn.c
@@ -6,22 +6,30 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 20:28:38 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/22 21:38:16 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/23 19:18:20 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 static int	is_spawn(char c)
-{
+{ //posicion inicial del jugador
 	return (c == 'N' || c == 'S' || c == 'E' || c == 'W');
+} //esa posicion sera reemplazada despues por suelo (0)
+
+static void	store_spawn(t_game *game, int x, int y)
+{
+	game->map.spawn_x = x;
+	game->map.spawn_y = y;
+	game->map.spawn_dir = game->map.grid[y][x];
+	game->map.grid[y][x] = '0';
 }
 
 int	find_and_store_spawn(t_game *game)  //encontrar al jugador
 {
 	int	x;
 	int	y;
-	int	count; //cuantos spawn se han encontrado
+	int	count; //contador de spawn (solo debe haber 1)
 
 	count = 0;
 	y = 0;
@@ -32,17 +40,15 @@ int	find_and_store_spawn(t_game *game)  //encontrar al jugador
 		{
 			if (is_spawn(game->map.grid[y][x])) //si encuentras N S E W
 			{   //guardas la posicion inicial (q no debe perderse)
-				game->map.spawn_x = x;
-				game->map.spawn_y = y;
-				game->map.spawn_dir = game->map.grid[y][x];
-				game->map.grid[y][x] = '0'; //conviertes a sueo '0' (para que validate_map no tenga q aceptar NSEW)
-				count++;                    //raycast tratara spawn como suelo
+				//guardas las coordenadas (es direccion spawn_dir)
+				store_spawn(game, x, y); //conviertes a suelo '0' (para que validate_map no tenga q aceptar NSEW)
+				count++;                 //raycast tratara spawn como suelo
 			}
 			x++;
 		}
 		y++;
 	}
-	if (count != 1)
-    	return (1);
-	return (0); //correcto un solo spawn
+	if (count != 1) // si hay 0 o mas de un spawn, error
+		return (1);
+	return (0); //lo correcto, un solo spawn
 }
--- src/parser/parse_file.c
+++ src/parser/parse_file.c
@@ -6,13 +6,13 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 15:51:47 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/22 20:22:20 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/23 19:22:10 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-//funcion que coordina todo
+//Director de orquesta: coordina todo
 int	parse_file(const char *filename, t_game *game)
 {
 	int		fd;
@@ -21,27 +21,22 @@ int	parse_file(const char *filename, t_game *game)
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (1);
-
 	if (read_map_lines(fd, &arr) != 0) //lee y guarda las lineas del mapa en arr
 	{
 		close(fd);
 		return (1);
 	}
 	close(fd);
-
 	if (build_grid(game, &arr) != 0)
 	{
 		free_lines(&arr);
 		return (1);
 	}
 	free_lines(&arr);
-
 	if (find_and_store_spawn(game) != 0)
 		return (1);
-
 	if (validate_map(game) != 0)
-    	return (1);
-
+		return (1);
 	return (0); //todo OK
 }
 
