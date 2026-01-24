d4962a4804f4796d24adc0df6ca9a986c7a781f9
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-01-24 21:34:01 +0100

    parser_header

--- src/parser/parse_file.c
+++ src/parser/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 15:51:47 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/24 18:37:15 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/24 21:32:59 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@ int	parse_file(const char *filename, t_game *game)
 	int		fd;
 	t_lines	arr; //es un buffer temporal de parseo
 
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (1);
+	if (parse_header(fd, game) != 0)
+	{
+		close(fd);
+		return (1);
+	}
+	close(fd);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (1);
@@ -39,4 +48,3 @@ int	parse_file(const char *filename, t_game *game)
 		return (1);
 	return (0); //todo OK
 }
-
