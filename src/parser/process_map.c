2245205eb4cbbb84b91daa1244b8a0535ff7c470
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-01-24 17:43:25 +0100

    process_map

--- src/parser/process_map.c
+++ src/parser/process_map.c
@@ -6,50 +6,50 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 15:56:22 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/24 16:14:08 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/24 17:42:58 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "get_next_line.h"
-
+//Se encarga de ignorar líneas antes del mapa (cabecera)
 static int	handle_until_map(int *in_map, char **line, int fd)
 {
-	if (*in_map == 1)
+	if (*in_map == 1) //estoy dentro del mapa, no hago nada
 		return (0);
-	if (is_map_line(*line))
+	if (is_map_line(*line)) //estaba fuera pero encuentro una linea del mapa, lo marco
 	{
 		*in_map = 1;
 		return (0);
 	}
-	free(*line);
+	free(*line); //no he llegado al mapa, sigo leyendo...
 	*line = get_next_line(fd);
 	return (1);
 }
-
+//Procesa una línea mientras el mapa no ha terminado (end_map == 0)
 static int	handle_map_body(t_lines *arr, char *line, int *end_map)
 {
 	if (is_blank_line(line))
 	{
-		*end_map = 1;
+		*end_map = 1; //marca q ha terminado el mapa
 		free(line);
 		return (0);
 	}
-	if (is_map_row(line) == 0)
+	if (is_map_row(line) == 0) //comprueba si la linea es valida
 	{
-		free(line);
+		free(line); 
 		free_lines(arr);
 		return (1);
 	}
-	if (lines_push(arr, line) != 0)
+	if (lines_push(arr, line) != 0) //guarda la linea valida
 	{
-		free(line);
+		free(line); //si falla push (malloc), limpias
 		free_lines(arr);
 		return (1);
 	}
 	return (0);
 }
-
+//despues del mapa, solo es valido lineas en blanco, cualquier otra cosa seria un error
 static int	handle_after_map(t_lines *arr, char *line)
 {
 	if (is_blank_line(line) == 0)
@@ -58,10 +58,10 @@ static int	handle_after_map(t_lines *arr, char *line)
 		free_lines(arr);
 		return (1);
 	}
-	free(line);
+	free(line); //lineas en blanco post al mapa, se ignoran
 	return (0);
 }
-
+//analisis de las lineas antes, durante y tras el mapa
 int	process_map_step(t_parse_state *st, t_lines *arr)
 {
 	if (handle_until_map(&st->in_map, &st->line, st->fd))
