6981e91b698bc9611d3c815e8f00bd7746d6bb59
Patricia Quesada Duenas <patquesa@c3r1s2.42malaga.com>
2026-01-26 11:16:36 +0100

    refactorizar process_map

--- src/parser/process_map.c
+++ src/parser/process_map.c
@@ -6,101 +6,83 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 15:56:22 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/26 09:56:13 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/26 11:15:31 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "get_next_line.h"
-//Se encarga de ignorar líneas antes del mapa (cabecera)
-static int	handle_until_map(int *in_map, const char *line)
+
+static int	handle_until_map(int *in_map, const char *line) //ignora lineas anteriores al mapa
 {
 	if (*in_map)
 		return (0);
 	if (is_blank_line(line))
 		return (1);
-	if (is_map_row(line)) // <-- IMPORTANTE: row, no line
+	if (is_map_row(line))
 	{
 		*in_map = 1;
 		return (0);
 	}
-	return (1); // cabecera/ruido antes del mapa: se ignora
+	return (1); //cabecera antes del mapa se ignora
 }
 
-static int	handle_map_body(t_lines *arr, char *line, int *end_map, int *consumed)
+static int	handle_map_body(t_lines *arr, char **line, int *end_map)
 {
-	int	*consumed;
-
-	consumed = 0;
-	if (is_blank_line(line))
+	if (is_blank_line(*line))
 	{
 		*end_map = 1;
-		return (0); //no se guarda
+		free(*line);
+		return (0);
 	}
-	if (is_map_row(line) == 0)
+	if (is_map_row(*line) == 0)
 	{
+		free(*line);
 		free_lines(arr);
 		return (1);
 	}
-	if (lines_push(arr, line) != 0)
+	if (lines_push(arr, *line) != 0)
 	{
+		free(*line);
 		free_lines(arr);
 		return (1);
 	}
-	*consumed = 1; // arr se queda con line
+	*line = NULL;
 	return (0);
 }
 
-//despues del mapa, solo es valido lineas en blanco, cualquier otra cosa seria un error
-static int	handle_after_map(t_lines *arr, char *line)
+static int	handle_after_map(t_lines *arr, char **line) //despues del mapa, solo es valido lineas en blanco, cualquier otra cosa seria un error
 {
-	if (is_blank_line(line) == 0)
+	if (is_blank_line(*line) == 0)
 	{
-		//fprintf(stderr, "[MAP] NON-blank after map: '%s'\n", line);
-		//free(line);
+		free(*line);
 		free_lines(arr);
 		return (1);
 	}
-	//fprintf(stderr, "[MAP] blank after map (ok)\n");
-	//free(line); //lineas en blanco post al mapa, se ignoran
+	free(*line);
 	return (0);
 }
 
-//analisis de las lineas antes, durante y tras el mapa
-int	process_map_step(t_parse_state *st, t_lines *arr)
+int	process_map_step(t_parse_state *st, t_lines *arr) //analisis de las lineas antes, durante y tras el mapa
 {
-	int	consumed;
+	int	ret;
 
 	if (!st->line)
 		return (0);
-	if (handle_until_map(&st->in_map, st->line)) /* ANTES DEL MAPA */
+	if (handle_until_map(&st->in_map, st->line))
 	{
 		free(st->line);
 		st->line = get_next_line(st->fd);
 		return (0);
 	}
-	if (st->end_map == 0) /* DURANTE / DESPUÉS DEL MAPA */
-	{
-		if (handle_map_body(arr, st->line, &st->end_map, &consumed) != 0)
-		{
-			free(st->line);
-			st->line = NULL;
-			return (1);
-		}
-		if (consumed)
-			st->line = NULL;/* arr ya es dueño de la línea */
-		else
-			free(st->line);
-	}
+	if (st->end_map == 0)
+		ret = handle_map_body(arr, &st->line, &st->end_map);
 	else
+		ret = handle_after_map(arr, &st->line);
+	if (ret != 0)
 	{
-		if (handle_after_map(arr, st->line) != 0)
-		{
-			free(st->line);
-			st->line = NULL;
-			return (1);
-		}
-		free(st->line);
+		st->line = NULL;
+		return (1);
 	}
 	st->line = get_next_line(st->fd);
 	return (0);
