45c7ed48ed122570720250cab0f80847094f0d83
Patricia Quesada Duenas <patquesa@c3r7s1.42malaga.com>
2026-01-26 15:29:28 +0100

    eliminar funcion repe

--- src/parser/parse_header.c
+++ src/parser/parse_header.c
@@ -6,7 +6,7 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/25 12:53:56 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/26 13:10:38 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/26 15:28:55 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,11 @@ static int	header_complete(t_game *g) //comprueba que header este completo (cd e
 		&& g->cfg.floor_set && g->cfg.ceiling_set);
 }
 
-/*Convierte RGB a uint32 RGBA (útil para MLX42 al pintar) 
-static uint32_t	rgb_to_rgba_u32(int r, int g, int b)
-{
-	return ((uint32_t)r << 24) | ((uint32_t)g << 16) | ((uint32_t)b << 8) | 255u;
-}*/
-
 static int	free_and_return(char *line, int ret)
 {
 	free(line);
 	return (ret);
 }
-
 
 static int	process_header_step(int fd, t_game *game, char **raw) //raw puntero a linea actual
 {
