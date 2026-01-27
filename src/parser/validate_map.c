6771c90e78389d1e41c1e4ff787c39abcd66ec4b
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-01-27 12:43:24 +0100

    recuperar closed

--- src/parser/validate_map.c
+++ src/parser/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: patquesa <patquesa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 10:17:37 by patquesa          #+#    #+#             */
-/*   Updated: 2026/01/27 11:36:23 by patquesa         ###   ########.fr       */
+/*   Updated: 2026/01/27 12:39:09 by patquesa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,7 @@ static int	validate_flood(t_game *game) //flood va marcando las casillas Ya visi
 
 static int	is_closed_floor(t_game *g, int y, int x)
 {
-	if (y == 0 || x == 0 || y == g->map.height - 1 || x == g->map.width - 1)
+	if (y == 0 || x == 0 || y == g->map.height - 1 || x == g->map.width - 1) //si un 0 (suelo pisable) esta en borde, error
 		return (0);
 	if (g->map.grid[y - 1][x] == ' ' || g->map.grid[y + 1][x] == ' '
 		|| g->map.grid[y][x - 1] == ' ' || g->map.grid[y][x + 1] == ' ')
@@ -130,33 +130,3 @@ int	validate_map(t_game *game)
 		return (1);
 	return (0);
 }
-
-
-
-/*
-int	validate_map(t_game *game)
-{
-	int		y;
-	int		x;
-	char	c;
-
-	if (!game || !game->map.grid || game->map.height <= 0 || game->map.width <= 0) //si grid es NULL
-		return (1);
-	y = 0;
-	while (y < game->map.height)
-	{
-		x = 0;
-		while (x < game->map.width)
-		{
-			c = game->map.grid[y][x];
-			if (!is_valid_cell(c))
-				return (1);
-			if (c == '0' && !is_closed_floor(game, y, x))
-				return (1);
-			x++;
-		}
-		y++;
-	}
-	return (0);
-}
-*/
